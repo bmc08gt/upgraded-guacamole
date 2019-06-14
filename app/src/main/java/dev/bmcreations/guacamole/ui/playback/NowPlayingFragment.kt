45f171c521af9f561f6256f03cce5a5b4ac2afd5
Brandon McAnsh <brandon.mcansh@gmail.com>
2019-06-14 16:31:29 -0400

    NowPlaying: Add stateful-ness to mini player
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/guacamole/ui/playback/NowPlayingFragment.kt
+++ app/src/main/java/dev/bmcreations/guacamole/ui/playback/NowPlayingFragment.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import dev.bmcreations.guacamole.R
-import dev.bmcreations.guacamole.extensions.getViewModel
-import dev.bmcreations.guacamole.extensions.gone
-import dev.bmcreations.guacamole.extensions.picasso
-import dev.bmcreations.guacamole.extensions.visible
+import dev.bmcreations.guacamole.extensions.*
 import dev.bmcreations.guacamole.ui.details.AlbumTrackEntity
 import dev.bmcreations.guacamole.ui.details.PlaylistTrackEntity
+import dev.bmcreations.guacamole.ui.details.TrackEntity
+import dev.bmcreations.guacamole.ui.playback.NowPlayingViewModel.State
 import dev.bmcreations.musickit.networking.api.models.isExplicit
 import dev.bmcreations.musickit.networking.api.models.urlWithDimensions
+import kotlinx.android.synthetic.main.now_playing_mini.*
 import kotlinx.android.synthetic.main.now_playing_mini.view.*
 
 class NowPlayingFragment: Fragment() {
@@ -36,55 +36,82 @@ class NowPlayingFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        play_pause.setOnClickListener { viewModel?.playPause() }
         observe()
     }
 
     private fun observe() {
-        viewModel?.selectedTrack?.observe(viewLifecycleOwner, Observer { entity ->
-            when (entity) {
-                null -> root.gone()
-                else -> {
-                    root.visible().also {
-                        when (entity) {
-                            is PlaylistTrackEntity -> {
-                                root.track_name.text = entity.track.attributes?.name
-//                                    track_artist.text = entity.track.attributes?.artistName
-                                if (entity.track.attributes?.isExplicit == true) {
-                                    root.explicit.visible()
-                                } else {
-                                    root.explicit.gone()
-                                }
-                                picasso {
-                                    root.track_art.visible()
-                                    it.cancelRequest(root.track_art)
-                                    it.load(entity.track.attributes?.artwork?.urlWithDimensions)
-                                        .resize(72, 72)
-                                        .placeholder(R.drawable.ic_music_fail)
-                                        .error(R.drawable.ic_music_fail)
-                                        .into(root.track_art)
-                                }
-                            }
-                            is AlbumTrackEntity -> {
-                                root.track_name?.text = entity.track.attributes?.name
-                                if (entity.track.attributes?.isExplicit == true) {
-                                    root.explicit.visible()
-                                } else {
-                                    root.explicit.gone()
-                                }
-                                picasso {
-                                    root.track_art.visible()
-                                    it.cancelRequest(root.track_art)
-                                    it.load(entity.track.attributes?.artwork?.urlWithDimensions)
-                                        .resize(72, 72)
-                                        .placeholder(R.drawable.ic_music_fail)
-                                        .error(R.drawable.ic_music_fail)
-                                        .into(root.track_art)
-                                }
-                            }
+        viewModel?.playState?.observe(viewLifecycleOwner, Observer { handlePlayState(it) })
+        viewModel?.selectedTrack?.observe(viewLifecycleOwner, Observer { handleTrackSelection(it) })
+    }
+
+    private fun handlePlayState(state: State) {
+        when (state) {
+            is State.Uninitialized -> root.gone()
+            else -> {
+                root.visible()
+                when (state) {
+                    is State.Initializing -> {
+                        root.play_pause.gone()
+                        root.loading.visible()
+                    }
+                    is State.Playing -> {
+                        root.loading.gone()
+                        root.play_pause.visible().also {
+                            root.play_pause.setImageResource(R.drawable.ic_baseline_pause_black_24dp)
+                        }
+                    }
+                    is State.Paused -> {
+                        root.loading.gone()
+                        root.play_pause.visible().also {
+                            root.play_pause.setImageResource(R.drawable.ic_play_arrow_gray_32dp)
                         }
                     }
                 }
             }
-        })
+        }
+    }
+
+    private fun handleTrackSelection(entity: TrackEntity?) {
+        if (entity != null) {
+            when (entity) {
+                is PlaylistTrackEntity -> {
+                    root.track_name.text = entity.track.attributes?.name
+//                                    track_artist.text = entity.track.attributes?.artistName
+                    if (entity.track.attributes?.isExplicit == true) {
+                        root.explicit.visible()
+                    } else {
+                        root.explicit.gone()
+                    }
+                    picasso {
+                        root.track_art.visible()
+                        it.cancelRequest(root.track_art)
+                        it.load(entity.track.attributes?.artwork?.urlWithDimensions)
+                            .resize(72, 72)
+                            .placeholder(R.drawable.ic_music_fail)
+                            .error(R.drawable.ic_music_fail)
+                            .into(root.track_art)
+                    }
+                }
+                is AlbumTrackEntity -> {
+                    root.track_name?.text = entity.track.attributes?.name
+                    if (entity.track.attributes?.isExplicit == true) {
+                        root.explicit.visible()
+                    } else {
+                        root.explicit.gone()
+                    }
+                    picasso {
+                        root.track_art.visible()
+                        it.cancelRequest(root.track_art)
+                        it.load(entity.track.attributes?.artwork?.urlWithDimensions)
+                            .resize(72, 72)
+                            .placeholder(R.drawable.ic_music_fail)
+                            .error(R.drawable.ic_music_fail)
+                            .into(root.track_art)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/dev/bmcreations/guacamole/ui/playback/NowPlayingViewModel.kt
+++ app/src/main/java/dev/bmcreations/guacamole/ui/playback/NowPlayingViewModel.kt
@@ -3,14 +3,28 @@ package dev.bmcreations.guacamole.ui.playback
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.bmcreations.guacamole.ui.details.TrackEntity
+import dev.bmcreations.guacamole.viewmodel.SingleLiveEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class NowPlayingViewModel private constructor(context: Context): ViewModel() {
 
     val selectedTrack:  MutableLiveData<TrackEntity?> = MutableLiveData()
+    val playState: MutableLiveData<State> = SingleLiveEvent()
+
+    sealed class State {
+        object Playing: State()
+        object Paused: State()
+        object Initializing: State()
+        object Uninitialized: State()
+    }
 
     init {
         selectedTrack.value = null
+        playState.value = State.Uninitialized
     }
 
     companion object Factory {
@@ -20,6 +34,26 @@ class NowPlayingViewModel private constructor(context: Context): ViewModel() {
     }
 
     fun updatePlayingTrack(track: TrackEntity?) {
+        playState.postValue(State.Initializing)
         selectedTrack.postValue(track)
+    }
+
+    fun playPause() {
+        playState.value?.let {
+            when (it) {
+                is State.Playing -> {
+                    playState.value = State.Paused
+                    // TODO: Fire off pause
+                }
+                is State.Paused -> {
+                    playState.value = State.Playing
+                    // TODO: Fire off play
+                }
+                is State.Uninitialized -> {
+                    playState.value = State.Initializing
+                    // TODO: Fire off play
+                }
+            }
+        }
     }
 }
