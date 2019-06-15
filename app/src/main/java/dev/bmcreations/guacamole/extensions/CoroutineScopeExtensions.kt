ed3e1ba69cf800bea89f56a45ad570e2a536ccec
Brandon McAnsh <brandon.mcansh@gmail.com>
2019-06-14 21:06:48 -0400

    Add extensions for helpful coroutine delayed Job's
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/guacamole/extensions/CoroutineScopeExtensions.kt
+++ app/src/main/java/dev/bmcreations/guacamole/extensions/CoroutineScopeExtensions.kt
@@ -1,8 +1,27 @@
 package dev.bmcreations.guacamole.extensions
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
+import java.util.*
 
 private val job = Job()
 internal val uiScope  = CoroutineScope(Dispatchers.Main + job)
+
+fun at(date: Date, callback: (() -> Unit)): Job {
+    return at(date.time, callback)
+}
+
+fun at(timestamp: Long, callback: (() -> Unit)): Job {
+    var waitTime = (timestamp - System.currentTimeMillis())
+    if (waitTime < 0) waitTime = 0
+    return uiScope.launch {
+        delay(waitTime)
+        callback.invoke()
+    }
+}
+
+fun inTime(offset: Long, callback: (() -> Unit)): Job {
+    return uiScope.launch {
+        delay(offset)
+        callback.invoke()
+    }
+}
