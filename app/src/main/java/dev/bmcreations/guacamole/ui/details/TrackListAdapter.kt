31bf98c212bd5558afcd3578af763354ff8e1e94
Brandon McAnsh <brandon.mcansh@gmail.com>
2019-07-09 17:46:31 -0400

    Add EQ visualization to AlbumDetail track entities for playlist and albums
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/guacamole/ui/details/TrackListAdapter.kt
+++ app/src/main/java/dev/bmcreations/guacamole/ui/details/TrackListAdapter.kt
@@ -4,11 +4,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import dev.bmcreations.guacamole.ui.details.TrackVH.Factory.ALBUM
 import dev.bmcreations.guacamole.ui.details.TrackVH.Factory.PLAYLIST
+import dev.bmcreations.guacamole.ui.playback.NowPlayingViewModel
 import dev.bmcreations.musickit.networking.api.models.AlbumTrackEntity
 import dev.bmcreations.musickit.networking.api.models.TrackEntity
 
 class TrackListAdapter : ListAdapter<TrackEntity, TrackVH>(TRACK_DATA_DIFF_CALLBACK) {
 
+    var nowPlaying: NowPlayingViewModel? = null
     var onTrackSelected: ((TrackVH) -> (Unit))? = null
 
     override fun getItemViewType(position: Int): Int {
@@ -23,6 +25,20 @@ class TrackListAdapter : ListAdapter<TrackEntity, TrackVH>(TRACK_DATA_DIFF_CALLB
     }
 
     override fun onBindViewHolder(holder: TrackVH, position: Int) {
-        holder.entity = getItem(position)
+        getItem(position).let { track ->
+            holder.entity = track
+            holder.hideVisualization()
+            nowPlaying?.selectedTrack?.value?.let {
+                if (track == it) {
+                    nowPlaying?.playState?.value?.let { state ->
+                        when (state) {
+                            NowPlayingViewModel.State.Playing -> holder.visualizePlayback()
+                            NowPlayingViewModel.State.Paused,
+                            NowPlayingViewModel.State.Initializing -> holder.pauseVisualization()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
